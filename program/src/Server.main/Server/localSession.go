361057c49a9c9df662d907ae06484bae66dcaa6e
wenkaizheng <31551859+wenkaizheng@users.noreply.github.com>
2020-05-02 15:05:34 -0700

    update: rewrite shakehand func and works correctly

--- program/src/Server.main/Server/localSession.go
+++ program/src/Server.main/Server/localSession.go
@@ -109,7 +109,7 @@ func (s *Session) readEncryptionTable(localTcpConn *net.TCPConn) error {
 	encode := make([]byte, 256)
 	decode := make([]byte, 256)
 	check1, check2 := Core.ReadAll(encode, localTcpConn, 256)
-	if (check1 == -1 && check2 != nil) || (check1 == 0 && check2 == nil) {
+	if (check1 == -1 && check2 != nil) {
 		s.ipMap.Delete(s.keyInMap)
 		return errors.New("Read encounters problem when read encode table")
 	}
@@ -134,10 +134,10 @@ func (s *Session) shakeHand(localTcpConn *net.TCPConn) error {
 		return errors.New("The server proxy is not running")
 	}
 	var serverTcpConn *net.TCPConn
-	request := make([]byte, 262)
+	request := make([]byte, 2)
 	// now we expect socks5 protocol, first step is confirm socks5
 	//readLength, err := localTcpConn.Read(request)
-	readLength, err := localTcpConn.Read(request)
+	readLength, err := Core.ReadAll(request,localTcpConn ,2)
 	// we need to use proxy to
 	if err != nil {
 		Logging.NormalLogger.Println("encounter error when reading")
@@ -149,13 +149,20 @@ func (s *Session) shakeHand(localTcpConn *net.TCPConn) error {
 	if decodedRequest[0] != 0x5 {
 		return errors.New("The protocol setting is not proxy5")
 	}
-
+	nextReadByte := int(decodedRequest[1])
+	request = make([]byte, nextReadByte)
+	readLength, err = Core.ReadAll(request,localTcpConn ,nextReadByte)
+	// we need to use proxy to
+	if err != nil {
+		Logging.NormalLogger.Println("encounter error when reading method")
+		return err
+	}
 	Logging.NormalLogger.Println("1st request", decodedRequest, "length'", readLength)
 
 	// give response to local and do not need key and password
 	encodeResponse := s.encryptionTable.Encode([]byte{0x5, 0x0})
 	Logging.NormalLogger.Println("going to write response")
-	_, err = localTcpConn.Write(encodeResponse)
+	_, err = Core.WriteAll(encodeResponse, localTcpConn, 2)
 	if err!= nil{
 		return err
 	}
@@ -168,45 +175,127 @@ func (s *Session) shakeHand(localTcpConn *net.TCPConn) error {
 		**/
 	Logging.NormalLogger.Println("waiting for new package")
 	// clean the buffer
-	request = make([]byte, 262)
-	readLength, err = localTcpConn.Read(request)
+	request = make([]byte, 4)
+	// second step is get the domain name and ip address from local
+	//readLength, err = localTcpConn.Read(request)
+	readLength, err = Core.ReadAll(request,localTcpConn ,4)
 	if err != nil {
 		return err
 	}
-	if readLength < 7 {
-		Logging.NormalLogger.Println("It should be at least seven bytes")
-		return errors.New("The packet should be at least seven bytes")
-	}
-	decodedRequest = s.encryptionTable.Decode(request[0:readLength])
-	Logging.NormalLogger.Println("request", decodedRequest, "length'", readLength)
-
+	length := 4
+	decodedRequest = s.encryptionTable.Decode(request[0:4])
 	// only support connect as method
 	if decodedRequest[1] != 0x1 {
-		return errors.New("Connect is only support method")
+		return  errors.New("100th connect is only support method")
+	}
+	if decodedRequest[3]==0x1{
+		// v4
+		length += 6
+		firstRequest := make([]byte, 6)
+		readLength, err = Core.ReadAll(firstRequest,localTcpConn ,6)
+		if err != nil {
+			return err
+		}
+		decodedRequest1 := s.encryptionTable.Decode(firstRequest[0:6])
+		realRequest := make([]byte, length)
+		j:=0
+		for i:=0;i<4;i++{
+			realRequest[j] = decodedRequest[i]
+			j+=1
+		}
+		for i:=0;i<6;i++{
+			realRequest[j]= decodedRequest1[i]
+			j+=1
+		}
+		tcpAddress := s.proxy.ConnectToRealServer(realRequest, length)
+		serverTcpConns, err := net.DialTCP("tcp", nil, tcpAddress)
 		
+		if err != nil {
+			return err
+		}
+		serverTcpConn = serverTcpConns
+
+	
+	}else if decodedRequest[3]==0x3{
+		// domain name
+		firstRequest := make([]byte, 1)
+		// read domain's length
+		readLength, err = Core.ReadAll(firstRequest,localTcpConn ,1)
+		if err != nil {
+			return err
+		}
+		length +=1
+		decodedRequest1 := s.encryptionTable.Decode(firstRequest[0:1])
+		nextReadByte = int(decodedRequest1[0])+2
+		length +=nextReadByte
+        secondRequest := make([]byte, nextReadByte)
+		readLength, err = Core.ReadAll(secondRequest,localTcpConn ,nextReadByte)
+		if err != nil {
+			return err
+		}
+		decodedRequest2 := s.encryptionTable.Decode(secondRequest[0:nextReadByte])
+		realRequest := make([]byte, length)
+		j:=0
+		for i:=0;i<4;i++{
+			realRequest[j] = decodedRequest[i]
+			j+=1
+		}
+		realRequest[j]= decodedRequest1[0]
+		j+=1
+		for i:=0;i<nextReadByte;i++{
+			realRequest[j]= decodedRequest2[i]
+			j+=1
+		}
+		Logging.NormalLogger.Println(realRequest)
+		tcpAddress := s.proxy.ConnectToRealServer(realRequest, length)
+		serverTcpConns, err := net.DialTCP("tcp", nil, tcpAddress)
+		if err != nil {
+			return err
+		}
+		serverTcpConn = serverTcpConns
+
+	}else if decodedRequest[3]==0x4{
+			// v6
+			
+			length += 18
+			firstRequest := make([]byte, 18)
+			readLength, err = Core.ReadAll(firstRequest,localTcpConn ,18)
+			if err != nil {
+				return err
+			}
+			decodedRequest1 := s.encryptionTable.Decode(firstRequest[0:18])
+			realRequest := make([]byte, length)
+			j:=0
+			for i:=0;i<4;i++{
+				realRequest[j] = decodedRequest[i]
+				j+=1
+			}
+			for i:=0;i<18;i++{
+				realRequest[j]= decodedRequest1[i]
+				j+=1
+			}
+			tcpAddress := s.proxy.ConnectToRealServer(realRequest, length)
+			serverTcpConns, err := net.DialTCP("tcp", nil, tcpAddress)
+			// add defer close
+			
+			if err != nil {
+			   return err
+			}
+			serverTcpConn = serverTcpConns
+			
 	}
 
-	tcpAddress := s.proxy.ConnectToRealServer(decodedRequest, readLength)
-	serverTcpConn, err = net.DialTCP("tcp", nil, tcpAddress)
-	if err != nil {
-		return err
-	}
 
 	response2 := []byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 	encodeResponse = s.encryptionTable.Encode(response2)
 	//localTcpConn.Core.WriteAll(encodeResponse)
-	_, err = localTcpConn.Write(encodeResponse)
-	if err != nil {
-		return err
-	}
-
-	connection := Core.NewConnectionHandler(localTcpConn, serverTcpConn, s.proxy.GetDevice(), s.encryptionTable)
+	_,err = Core.WriteAll(encodeResponse, localTcpConn, 10)
+    connection := Core.NewConnectionHandler(localTcpConn, serverTcpConn, s.proxy.GetDevice(), s.encryptionTable)
 	s.connections.Store(&connection, &connection)
 	go func() {
-		connection.TransferData()
-		s.connections.Delete(&connection)
+			connection.TransferData()
+			s.connections.Delete(&connection)
 	}()
-
 	return err
 }
 
